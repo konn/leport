9fbcec3454e903efcbaad313a2906dc457859b5f
Hiromi Ishii <konn.jinro@gmail.com>
2015-08-03 17:47:41 +0900

    Implemented leport-worker

--- leport-web/app/leport-worker.hs
+++ leport-web/app/leport-worker.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE NoImplicitPrelude, PatternGuards #-}
 module Main where
-import Control.Distributed.Process
-import LXC
-import Prelude
+import ClassyPrelude
+import Control.Distributed.Process.Backend.SimpleLocalnet
+import RPC                                                (remoteTable)
 
 main :: IO ()
-main = undefined
+main = do
+  args <- getArgs
+  let (host, port)
+        | [p] <- args = ("localhost", p)
+        | [h, p] <- args = (h, p)
+  be <- initializeBackend (unpack host) (unpack port) remoteTable
+  startSlave be
